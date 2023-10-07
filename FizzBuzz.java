22637401d9515f76fd63639d7e7f51eadefc4d6e
kazutaka <k89z10@yahoo.co.jp>
2023-10-07 17:35:57 +0900

    9. 文法 - 基本 1　課題

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+public class FizzBuzz {
+    public static void main(String[] args){
+        if(args.length ==0){
+            return;
+        }
+        int max = Integer.parseInt(args[0]);
+        String result = null;
+        for (int i = 1; i <= max; i++){
+            result ="";
+            if(num % 3 == 0){
+                result = result + "Fizz"; 
+            }
+            if (num % 5 == 0){
+                result = result + "Buzz";
+            }
+            if (result.length() == 0) {
+				result = result + num;
+			}
+			System.out.print(result + " ");
+        }
+    }
+}
